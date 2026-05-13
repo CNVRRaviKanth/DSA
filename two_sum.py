0c79eff9d0399147d9a5ae82ef12b796fde4dac2
Ravikanth <ravikanthapple489@gmail.com>
2026-05-13 16:48:11 +0530

    Two sum using hashmap

---
+++ two_sum.py
@@ -0,0 +1,15 @@
+def two_sum(arr,target):
+    hashmap = dict()
+    for i in range(len(arr)):
+        complement = target-arr[i]
+        if complement in hashmap:
+            return "pair found"
+        
+        hashmap[arr[i]] = i
+
+    return "pair not found"
+
+arr = [2, 7, 11, 15]
+target = 9
+ans = two_sum(arr,target)
+print(ans)
